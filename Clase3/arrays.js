37151f79ec3ceca2170668795ac5a245b2bdcf47
Ulises Flores <ulises.fg.98@gmail.com>
2024-01-16 21:33:56 -0600

    add Clase 3

---
+++ Clase3/arrays.js
@@ -0,0 +1,8 @@
+function esPalindromo(palabra) {
+  let palabraSinEspacios = palabra.toLowerCase().replace(/\s/g, '');
+  let palabraReversa = palabraSinEspacios.split('').reverse().join('');
+  return palabraSinEspacios === palabraReversa;
+}
+
+console.log(esPalindromo("reconocer"));
+console.log(esPalindromo("hola"));
